0e4c794a485ab4dbb5bd9bd55d88d98f60918117
codingABI <50620368+codingABI@users.noreply.github.com>
2023-01-04 19:33:59 +0100

    Update Sender5.ino

--- Sender5/Sender5.ino
+++ Sender5/Sender5.ino
@@ -2,7 +2,7 @@
  * Project: Sender5 (Ding15)
  * Description:
  * Sensor for the slot of a mailbox. When the slot is opened, a magenetic reed sensor 
- * triggers and sends a signal LoRa to the receiver. Additionally once per day the 
+ * triggers and sends a LoRa signal to the receiver. Additionally once per day the 
  * current battery voltage and the magnetic reed switch state will also be sent to the receiver.
  *
  * License: 2-Clause BSD License
