e3c1e5b9b0d1bb2537d14e87d3ef55f8db322bf8
codingABI <50620368+codingABI@users.noreply.github.com>
2023-01-07 19:42:14 +0100

    Add files via upload

---
+++ Sender1/externalCode.ino
@@ -0,0 +1,28 @@
+// Measure Vcc (from https://forum.arduino.cc/t/measuring-battery-voltage-conditionally/319327/5)
+int getBandgap(void) // Returns actual value of Vcc (x 100)
+    {
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+     // For mega boards
+     const long InternalReferenceVoltage = 1115L;  // Adjust this value to your boards specific internal BG voltage x1000
+        // REFS1 REFS0          --> 0 1, AVcc internal ref. -Selects AVcc reference
+        // MUX4 MUX3 MUX2 MUX1 MUX0  --> 11110 1.1V (VBG)         -Selects channel 30, bandgap voltage, to measure
+     ADMUX = (0<<REFS1) | (1<<REFS0) | (0<<ADLAR)| (0<<MUX5) | (1<<MUX4) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (0<<MUX0);
+  
+#else
+     // For 168/328 boards
+     const long InternalReferenceVoltage = 1086L;  // Adjust this value to your boards specific internal BG voltage x1000
+        // REFS1 REFS0          --> 0 1, AVcc internal ref. -Selects AVcc external reference
+        // MUX3 MUX2 MUX1 MUX0  --> 1110 1.1V (VBG)         -Selects channel 14, bandgap voltage, to measure
+     ADMUX = (0<<REFS1) | (1<<REFS0) | (0<<ADLAR) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (0<<MUX0);
+       
+#endif
+     delay(50);  // Let mux settle a little to get a more stable A/D conversion
+        // Start a conversion  
+     ADCSRA |= _BV( ADSC );
+        // Wait for it to complete
+     while( ( (ADCSRA & (1<<ADSC)) != 0 ) );
+        // Scale the value
+     int results = (((InternalReferenceVoltage * 1024L) / ADC) + 5L) / 10L; // calculates for straight line value 
+     return results;
+ 
+    }
