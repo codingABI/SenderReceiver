9de0feac5c2cf975ad261dc743f11b4af0f85594
codingABI <50620368+codingABI@users.noreply.github.com>
2023-01-20 23:14:20 +0100

    Create secrets.h

---
+++ Receiver/secrets.h
@@ -0,0 +1,11 @@
+// File for data which should not be published to the public
+
+// OTA password
+#define OTAPASSWORD "yourdata"
+
+const char g_eepromenc[] = "yourdata"; // Key for encrypting sensible data in eeprom
+
+// Blynk
+#define BLYNK_TEMPLATE_ID           "yourdata"
+#define BLYNK_DEVICE_NAME           "yourdata"
+#define BLYNK_AUTH_TOKEN            "yourdata"
